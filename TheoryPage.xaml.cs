76cafeea5ec5de4086b3f2d3446da7542154390a
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2024-10-11 03:41:55 +0500

    Create AI button

--- TheoryPage.xaml.cs
+++ TheoryPage.xaml.cs
@@ -11,23 +11,28 @@ public partial class TheoryPage
     {
         
     }
+    
+    private async void OnAskingAiClicked(object sender, EventArgs e)
+    {
+        
+    }
 
-    private async void OnNotesPage(object sender, EventArgs e)
+    private async void OnNotesPageClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new NotesPage());
     }
 
-    private async void OnCoursesPage(object sender, EventArgs e)
+    private async void OnCoursesPageClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CoursesPage());
     }
 
-    private async void OnTheoryPage(object sender, EventArgs e)
+    private async void OnTheoryPageClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new TheoryPage());
     }
 
-    private async void OnMetronomePage(object sender, EventArgs e)
+    private async void OnMetronomePageClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MetronomePage());
     }
