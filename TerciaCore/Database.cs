a1c5868a731dc6eba7a59abc8bc448a4aeba5238
usiqwerty <benlogin1@protonmail.com>
2024-11-18 20:36:27 +0500

    Use cache for api requests

--- TerciaCore/Database.cs
+++ TerciaCore/Database.cs
@@ -1,21 +1,31 @@
 ﻿using System.Net.Http.Json;
-
 namespace Tercia.TerciaCore;
 
 public static class Database
 {
-    private static HttpClient _httpClient = new()
-    {
-        BaseAddress = new Uri("http://130.193.58.142:8000")
-    };
+    private static readonly HttpClient HttpClient = new() { BaseAddress = new Uri("http://130.193.58.142:8000") };
+    private static List<Course>? _courses;
+    private static readonly Dictionary<int, List<Lesson>> Lessons = new();
 
-    public static async Task<List<Lesson>?> GetCourseLessons(int courseId)
+    public static async Task<List<Lesson>> GetCourseLessons(int courseId)
     {
-        return await _httpClient.GetFromJsonAsync<List<Lesson>>("/get-course-lessons");
+        if (!Lessons.ContainsKey(courseId))
+        {
+            var response = await HttpClient.GetFromJsonAsync<List<Lesson>>($"/get-course-lessons?lesson_id={courseId}");
+            if (response is null)
+                throw new Exception($"Could not fetch lessons for course {courseId}");
+            Lessons[courseId] = response;
+        }
+
+        return Lessons[courseId];
     }
 
     public static async Task<List<Course>?> GetAllCourses()
     {
-        return await _httpClient.GetFromJsonAsync<List<Course>>("/get-courses");
+        if (_courses is null)
+            _courses = await HttpClient.GetFromJsonAsync<List<Course>>("/get-courses");
+        if (_courses is null)
+            throw new Exception("Could not fetch courses list");
+        return _courses;
     }
 }
