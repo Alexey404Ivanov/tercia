a3f61803920ca2a1dba75295282106016f1d2159
usiqwerty <benlogin1@protonmail.com>
2024-12-07 10:30:10 +0500

    Use camel-case naming in API

--- TerciaCore/Lesson.cs
+++ TerciaCore/Lesson.cs
@@ -4,5 +4,5 @@ public class Lesson
 {
     public int Id { get; init; }
     public string Title { get; init; }
-    public string Video_Url { get; init; }
+    public string VideoUrl { get; init; }
 }
