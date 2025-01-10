7d60a4cc3cbce2ece3395c1e7f6d52b02976d1ab
usiqwerty <benlogin1@protonmail.com>
2025-01-10 19:54:46 +0500

    Fix video player size

--- Pages/Courses/ViewLessonPage.xaml.cs
+++ Pages/Courses/ViewLessonPage.xaml.cs
@@ -4,9 +4,16 @@ namespace Tercia.Pages.Courses;
 
 public partial class ViewLessonPage : ContentPage
 {
+    public double PlayerWidth => DeviceDisplay.MainDisplayInfo.Width;
+    public double PlayerHeight => PlayerWidth / 16.0 * 9;
+    public string VideoUrl { get; set; }
+    public string Title { get; set; }
+
     public ViewLessonPage(Lesson lesson)
     {
         InitializeComponent();
-        BindingContext = lesson;
+        VideoUrl = lesson.VideoUrl;
+        Title = lesson.Title;
+        BindingContext = this;
     }
 }
